2531e130f48a2a4773256de935926b8097aa178f
josepichardo-hash <rosbet75@gmail.com>
2026-07-07 16:22:59 -0600

    tests

---
+++ terraform/test.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "servidor_juice_shop" {
+  ami           = "ami-0c7217cdde317cfec" # ID de una AMI de Ubuntu 22.04 LTS en us-east-1 (verifica el ID según tu región)
+  instance_type = "t2.micro"             # Entra en la capa gratuita de AWS
+
+  tags = {
+    Name = "DevSecOps-JuiceShop-Server"
+  }
+}
